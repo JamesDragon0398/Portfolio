3a5627c251261c99be368ba2ac178aab0ccd256e
James <james@Jamess-MBP-2.attlocal.net>
2020-11-11 22:39:06 -0600

    Linux

---
+++ Linux/Solution_Script.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+rm -r Lucky_Duck_Investigations/ Dealer_Schedules_0310/ Roulette_Player_WinLoss_0310/
+
+mkdir -p Lucky_Duck_Investigations
+cd Lucky_Duck_Investigations
+mkdir -p Roulette_loss_Investigation
+cd Roulette_loss_Investigation
+mkdir -p Player_Analysis Dealer_Analysis Player_Dealer_Correlation
+touch Player_Analysis/Notes_Player_Analysis Dealer_Analysis/Notes_Dealer_Analysis Player_Dealer_Correlation/Notes_Player_Dealer_Correlation
+cd ../../
+
+mkdir -p Dealer_Schedules_0310
+mkdir -p Roulette_Player_WinLoss_0310
+
+cat <<EOF > Dealer_Schedules_0310/0310_Dealer_schedule
+
+cat <<EOF > Dealer_Schedules_0310/0311_Dealer_schedule
+
+cat <<EOF > Dealer_Schedules_0310/0312_Dealer_schedule
+
+cat <<EOF > Dealer_Schedules_0310/0313_Dealer_schedule
+
+cat <<EOF > Dealer_Schedules_0310/0314_Dealer_schedule
+
+cat <<EOF > Dealer_Schedules_0310/0315_Dealer_schedule
+
+cat <<EOF > Dealer_Schedules_0310/0316_Dealer_schedule
+
+cat <<EOF > Dealer_Schedules_0310/0317_Dealer_schedule
+
+
+cat <<EOF > Roulette_Player_WinLoss_0310/0310_win_loss_player_data
+
+
+cat <<EOF > Roulette_Player_WinLoss_0310/0311_win_loss_player_data
+
+
+cat <<EOF > Roulette_Player_WinLoss_0310/0312_win_loss_player_data
+
+
+cat <<EOF > Roulette_Player_WinLoss_0310/0313_win_loss_player_data
+
+
+cat <<EOF > Roulette_Player_WinLoss_0310/0314_win_loss_player_data
+
+
+cat <<EOF > Roulette_Player_WinLoss_0310/0315_win_loss_player_data
+
+
+cat <<EOF > Roulette_Player_WinLoss_0310/0316_win_loss_player_data
+
+
+cat <<EOF > Roulette_Player_WinLoss_0310/0317_win_loss_player_data
+
